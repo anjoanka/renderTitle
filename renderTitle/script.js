fda7416f764e060cdcd6206729f9180a08e2a801
Andriana Mykolaievych <72654463+anjoanka@users.noreply.github.com>
2023-04-12 16:54:13 +0300

    Add files via upload

---
+++ renderTitle/script.js
@@ -0,0 +1,31 @@
+/////завданн один
+
+// const data = {
+//     title: "some title from js"
+// }
+
+// const title = document.getElementById('yourTitle')
+// title.innerText = data.title
+
+
+
+////завдання два
+
+// const data2 = {
+//     titles: [
+//         "some first title from js",
+//         "some second title",
+//         "one more",
+//     ]
+// }
+
+// const titleElement = document.getElementById('yourTitle')
+
+// function updateTitle() {
+//     for (let i = 0; i < data2.titles.length; i++) {
+//         const currentTitle = data2.titles[i];
+//         titleElement.innerText = currentTitle;
+//         setTimeout(() => {}, 3000);    
+//     }
+// }
+// updateTitle();
